729ad5bfbb73ce2c265fceb0cdd49287fd6f3f32
Rktmmnrn <rfanomezaniavo@gmail.com>
2026-05-12 22:41:51 +0300

    feat: start learning

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hi you !!')
+})
+
+app.listen(port, () => {
+    console.log(`L'app écoute sur le port: ${port} actuellement`)
+})
