4a107a535e48f59536793109ac29f582c8450510
Rktmmnrn <rfanomezaniavo@gmail.com>
2026-05-15 19:25:08 +0300

    feat: connection with mysql

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'fenohery',
+    password: 'admin',
+    database: 'pretbancaire'
+})
+
+connection.connect();
 
 app.get('/', (req, res) => {
-    res.send('Hi you !!')
+    res.send('hi..!!');
 })
 
 app.listen(port, () => {
